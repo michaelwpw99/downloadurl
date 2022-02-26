1e585d0f3160a1b1f5e84110ce2c16d6289933dd
michaelwpw99 <79656987+michaelwpw99@users.noreply.github.com>
2022-02-26 11:26:23 +0000

    Add files via upload

--- featurescore.py
+++ featurescore.py
@@ -17,9 +17,8 @@ from sklearn import metrics
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
 
-
 def getTrainTestData(dataset,split):
-    np.random.seed(7)
+    np.random.seed(10)
     training = [] 
     testing = []
     np.random.shuffle(dataset)
@@ -37,6 +36,18 @@ def getTrainTestData(dataset,split):
     
     return training,testing
 
+data = read_csv('NEWDATASET.csv')
+feat = data.keys()
+feat_labels = np.asarray(feat)
+dataset = data.values
+np.random.shuffle(dataset)
+inst = 15500
+dataset = dataset[0:inst,:]
+train,test = getTrainTestData(dataset, 0.8)
+Xtrain = train[:,1:55] 
+ytrain = train[:,55] 
+Xtest = test[:,1:55] 
+ytest = test[:,55]
 
 
 def getAccuracy(pre,ytest): 
@@ -99,133 +110,138 @@ def convert2image(img):
     
     
 def csvtoImage(min_values, max_values):
-    data2 = read_csv('NEWDATASET_benign.csv')
+    benignpath = 'images/train/benign/'
+    malwarepath = 'images/train/malware/'
+    data2 = read_csv('traindata.csv')
     feat2 = data2.keys()
     feat_labels2 = np.asarray(feat2)
     csvdataset = data2.values
     csvdataset = csvdataset[0:15500,:]
     count = 0
     for row in csvdataset:
-        pixels = getpixels(row, min_values, max_values)
-        print(len(pixels))
+        pixels = getpixels(row[1:-1], min_values, max_values)
+        #print(len(pixels))
         img = convert2image(np.asarray(pixels))
-        cv2.imwrite('IMAGES_6x9/benign/benign' + str(count) + '.jpg',img)
-        count += 1
+        if row[-1] == 1:
+            cv2.imwrite(benignpath + str(count) + '.jpg',img)
+            count += 1
+        if row[-1] == 0:
+            cv2.imwrite(malwarepath + str(count) + '.jpg',img)
+            count += 1
+
+def traintest(train, test):
+    testcsv = 'testdata.csv'
+    traincsv = 'traindata.csv'
     
+    with open(traincsv, 'w') as f:
+        write = csv.writer(f)
+        write.writerows(train)
+    
+    with open(testcsv, 'w') as f:
+        write = csv.writer(f)
+        write.writerows(test)
+    
+def runalgorithms(Xtrain, ytrain, Xtest, ytest):
+    #traintest(train, test)
+    #print(ytest)
+    trees = 250
+    max_feat = 7
+    max_depth = 30
+    min_sample = 2
 
-data = read_csv('NEWDATASET.csv')
-feat = data.keys()
-feat_labels = np.asarray(feat)
-dataset = data.values
-np.random.shuffle(dataset)
-inst = 15500
-dataset = dataset[0:inst,:]
-train,test = getTrainTestData(dataset, 0.8)
-Xtrain = train[:,1:55] 
-ytrain = train[:,55] 
-shape = np.shape(Xtrain)
-print("Shape of the dataset ",shape)
-print("Size of Data set before feature selection: %.2f MB"%(Xtrain.nbytes/1e6))
+    clf = ske.RandomForestClassifier(n_estimators=trees, max_features=max_feat, 
+    max_depth=max_depth, min_samples_split= min_sample, 
+    random_state=0, n_jobs=-1)
 
-Xtest = test[:,1:55] 
-ytest = test[:,55]
-#print(ytest)
-trees = 250
-max_feat = 7
-max_depth = 30
-min_sample = 2
+    start = time.time() 
+    clf.fit(Xtrain, ytrain) 
+    end = time.time()
 
-clf = ske.RandomForestClassifier(n_estimators=trees, max_features=max_feat, 
-max_depth=max_depth, min_samples_split= min_sample, 
-random_state=0, n_jobs=-1)
+    #print(Xtrain)
+    #print(ytrain)
 
-start = time.time() 
-clf.fit(Xtrain, ytrain) 
-end = time.time()
+    print("Execution time for building the Tree is: %f"%(float(end)- float(start)))
+    pre = clf.predict(Xtest)
+    acc = getAccuracy(pre, ytest)
+    print("Accuracy of model before feature selection is %.2f"%(100*acc))
 
-#print(Xtrain)
-#print(ytrain)
-
-print("Execution time for building the Tree is: %f"%(float(end)- float(start)))
-pre = clf.predict(Xtest)
-acc = getAccuracy(pre, ytest)
-print("Accuracy of model before feature selection is %.2f"%(100*acc))
-
-newfeatures = []
-for feature in zip(feat_labels, clf.feature_importances_):
-    #print(feature)
-    #print(type(feature))
-    if feature[1] >= 0.01:
-        newfeatures.append(feature[0])
+    newfeatures = []
+    for feature in zip(feat_labels, clf.feature_importances_):
+        #print(feature)
+        #print(type(feature))
+        if feature[1] >= 0.01:
+            newfeatures.append(feature[0])
         
 
-#print(newfeatures)
-sfm = SelectFromModel(clf, threshold=0.01) 
+    #print(newfeatures)
+    sfm = SelectFromModel(clf, threshold=0.01) 
 
 
-sfm.fit(Xtrain,ytrain)
-Xtrain_1 = sfm.transform(Xtrain) 
-Xtest_1 = sfm.transform(Xtest)
+    sfm.fit(Xtrain,ytrain)
+    Xtrain_1 = sfm.transform(Xtrain) 
+    Xtest_1 = sfm.transform(Xtest)
 
-shape = np.shape(Xtrain_1)
-print("Shape of the dataset ",shape)
-
-
-start = time.time() 
-clf.fit(Xtrain_1, ytrain) 
-end = time.time()
-
-print("Execution time for building the Tree is: %f"%(float(end)- float(start)))
-pre = clf.predict(Xtest_1) 
-count = 0
-acc2 = getAccuracy(pre, ytest)
-print("Accuracy after feature selection %.2f"%(100*acc2))
+    shape = np.shape(Xtrain_1)
+    print("Shape of the dataset ",shape)
 
 
-algorithms = {
-        "DecisionTree": tree.DecisionTreeClassifier(max_depth=10),
-        "RandomForest": ske.RandomForestClassifier(n_estimators=50),
-        "GradientBoosting": ske.GradientBoostingClassifier(n_estimators=50),
-        "AdaBoost": ske.AdaBoostClassifier(n_estimators=100),
+    start = time.time() 
+    clf.fit(Xtrain_1, ytrain) 
+    end = time.time()
+
+    print("Execution time for building the Tree is: %f"%(float(end)- float(start)))
+    pre = clf.predict(Xtest_1) 
+    count = 0
+    acc2 = getAccuracy(pre, ytest)
+    print("Accuracy after feature selection %.2f"%(100*acc2))
+
+
+    algorithms = {
+            "DecisionTree": tree.DecisionTreeClassifier(max_depth=10),
+            "RandomForest": ske.RandomForestClassifier(n_estimators=50),
+            "GradientBoosting": ske.GradientBoostingClassifier(n_estimators=50),
+            "AdaBoost": ske.AdaBoostClassifier(n_estimators=100),
     }
         
-results = {}
-print("\nNow testing algorithms")
-for algo in algorithms:
-    clf = algorithms[algo]
-    clf.fit(Xtrain, ytrain)
-    score = clf.score(Xtest, ytest)
-    print("%s : %f %%" % (algo, score*100))
-    results[algo] = score
+    results = {}
+    print("\nNow testing algorithms")
+    for algo in algorithms:
+        clf = algorithms[algo]
+        clf.fit(Xtrain, ytrain)
+        score = clf.score(Xtest, ytest)
+        print("%s : %f %%" % (algo, score*100))
+        results[algo] = score
 
-winner = max(results, key=results.get)
+    winner = max(results, key=results.get)
 
-svmalg = svm.SVC(kernel='linear')
-svmalg.fit(Xtrain, ytrain)
-y_pred = svmalg.predict(Xtest)
-svmacc = metrics.accuracy_score(ytest, y_pred)
-print("SVM: %f %%" % (svmacc*100))
+    #svmalg = svm.SVC(kernel='linear')
+    #svmalg.fit(Xtrain, ytrain)
+    #y_pred = svmalg.predict(Xtest)
+    #svmacc = metrics.accuracy_score(ytest, y_pred)
+    #print("SVM: %f %%" % (svmacc*100))
 
-neigh = KNeighborsClassifier(n_neighbors=7, algorithm='ball_tree')
-neigh.fit(Xtrain, ytrain)
-ypred = neigh.predict(Xtest)
-knnacc = metrics.accuracy_score(ytest, ypred) 
-print("K-Nearest Neighbours: %f %%" % (knnacc*100))
+    neigh = KNeighborsClassifier(n_neighbors=7, algorithm='ball_tree')
+    neigh.fit(Xtrain, ytrain)
+    ypred = neigh.predict(Xtest)
+    knnacc = metrics.accuracy_score(ytest, ypred) 
+    print("K-Nearest Neighbours: %f %%" % (knnacc*100))
 
-nbayes = GaussianNB()
-nbayes.fit(Xtrain, ytrain)
-nbayes_pred = nbayes.predict(Xtest)
-nbayes_acc = metrics.accuracy_score(ytest, nbayes_pred)
-print("Naive Bayes: %f %%" % (nbayes_acc*100))
+    nbayes = GaussianNB()
+    nbayes.fit(Xtrain, ytrain)
+    nbayes_pred = nbayes.predict(Xtest)
+    nbayes_acc = metrics.accuracy_score(ytest, nbayes_pred)
+    print("Naive Bayes: %f %%" % (nbayes_acc*100))
 
 
 
 if __name__ == "__main__":
     #newcsvFile(Xtrain_1.tolist(), ytrain.tolist(), Xtest_1.tolist(), ytest.tolist(), newfeatures)
-    #min_values, max_values = getminmax('NEWDATASET.csv')
+    
+    #min_values, max_values = getminmax('traindata.csv')
     
     #csvtoImage(min_values, max_values)
-    print('hello')
+    runalgorithms(Xtrain, ytrain, Xtest, ytest)
+    print('done')
 
 
 
